b9cef0a652cdb32ea1501fc6b7ef7ee59ad6275d
Diya <124015506+Diya1805@users.noreply.github.com>
2023-02-24 08:45:56 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,15 @@
+
+function apap()
+
+{
+    document.getElementById("aa").src= "org.jpg";
+    document.getElementById("aa").style.opacity="1";
+}
+
+
+
+
+function mUp(obj) {
+
+  obj.innerHTML="Health Benefits of Oranges Protects your cells from damage.Helps your body make collagen, a protein that heals wounds and gives you smoother skin.makes it easier to absorb iron to fight anemia.Boosts your immune system, your body's defense against germs";
+}
